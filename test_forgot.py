a574d6f9fd3cdf9d0feaa4b2d1d08b06d2306a76
navya-chengelli <160773093+navya-chengelli@users.noreply.github.com>
2024-06-25 21:24:29 +0530

    Add files via upload

--- test_forgot.py
+++ test_forgot.py
@@ -2,39 +2,56 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
-# # Navigate to IMDb search page
-class orange:
-    def __init__(self,url):
-        self.url=url
+class OrangeHRM:
+    def __init__(self, url):
+        self.url = url
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     def boot(self):
         self.driver.get(self.url)
-        sleep(3)
+        sleep(3)  # Wait for 3 seconds to load page completely
         self.driver.maximize_window()
 
     def quit(self):
+        """
+        Quit the WebDriver instance.
+        """
         self.driver.quit()
+
     def forgot_password(self):
-        self.boot()
-        sleep(3)
-
+        self.boot()  # Start the application and maximize window
+        sleep(3)  # Wait for 3 seconds
+        
+        # Set implicit wait to 60 seconds 
         self.driver.implicitly_wait(60)
-        forgot_password = self.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[4]/p').click()
 
-        sleep(2)
+        # Click on the 'Forgot your password?' link
+        forgot_password_link = self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[4]/p')
+        forgot_password_link.click()
+
+        sleep(2)  #wait for 2 seconds
+
+        # Enter username ('Admin') in the input field
         enter_username = self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/div/form/div[1]/div/div[2]/input')
         enter_username.send_keys("Admin")
-        sleep(2)
-        reset_password = self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/div/form/div[2]/button[2]').click()
-        sleep(1)
-        print("Reset password link sent sucessfully")
-        sleep(3)
-        self.quit()
 
-obj = orange("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        sleep(2)  
+
+        # Click on the 'Reset Password' button
+        reset_password_button = self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/div/form/div[2]/button[2]')
+        reset_password_button.click()
+
+        sleep(1) 
+        print("Reset password link sent successfully")
+
+        sleep(3)  # Wait for 3 seconds before quit
+        self.quit()  # Quit 
+
+# Create an instance of OrangeHRM with the login URL
+obj = OrangeHRM("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+# Execute the forgot_password method to demonstrate the 'Forgot Password'
 obj.forgot_password()
+
