9cf6e273efdb940c1aedcb59c1856db225b45155
navya-chengelli <160773093+navya-chengelli@users.noreply.github.com>
2024-06-25 21:17:53 +0530

    Add files via upload

--- menu_admin.py
+++ menu_admin.py
@@ -4,6 +4,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class AdminPage:
     def __init__(self, driver):
+
+       # Initialize AdminPage object with the WebDriver instance and menu items.
+
         self.driver = driver
         self.admin_menu_items = {
             "Admin": (By.ID, "menu_admin_viewAdminModule"),
@@ -22,9 +25,10 @@ class AdminPage:
     def verify_menu_items_displayed(self):
         for item_name, locator in self.admin_menu_items.items():
             try:
+                # Wait until the element specified by 'locator' is visible on the page
                 element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+                # Check if the element is displayed
                 assert element.is_displayed(), f"{item_name} menu item is not displayed"
                 print(f"{item_name} menu item is displayed")
             except AssertionError as e:
                 print(e)
-
